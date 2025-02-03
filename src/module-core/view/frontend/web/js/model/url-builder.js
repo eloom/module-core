14999a931bbf1aad280c656f8021c5fdd724f291
élOOm <contato@eloom.com.br>
2025-02-03 15:15:11 -0300

    Magento 2 support

---
+++ src/module-core/view/frontend/web/js/model/url-builder.js
@@ -0,0 +1,46 @@
+define(['jquery'], function ($) {
+    'use strict';
+
+    return {
+        method: 'rest',
+        storeCode: window.eloom.core.store.code,
+        version: 'V1',
+        serviceUrl: ':method/:storeCode/:version',
+
+        /**
+         * @param {String} url
+         * @param {Object} params
+         * @return {*}
+         */
+        createUrl: function (url, params) {
+            let completeUrl = this.serviceUrl + url;
+            return this.bindParams(completeUrl, params);
+        },
+
+        /**
+         * @param {String} url
+         * @param {Object} params
+         * @return {*}
+         */
+        bindParams: function (url, params) {
+            let urlParts;
+
+            params.method = this.method;
+            params.storeCode = this.storeCode;
+            params.version = this.version;
+
+            urlParts = url.split('/');
+            urlParts = urlParts.filter(Boolean);
+
+            $.each(urlParts, function (key, part) {
+                part = part.replace(':', '');
+
+                if (params[part] != undefined) {
+                    urlParts[key] = params[part];
+                }
+            });
+
+            return urlParts.join('/');
+        }
+    };
+});
